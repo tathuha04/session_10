1589d0bdc82a0e7ec143f853a8c45ca20d597388
TaThuHa <thuhak2tltnpt@gmail.com>
2022-11-10 09:46:16 +0700

    update

--- baitapth/bai1_2.js
+++ baitapth/bai1_2.js
@@ -1,6 +1,8 @@
-
+let inputWidth;
+let inputHeight;
 inputWidth = prompt("Enter the width");
-inputHeight = prompt("Enter the height"); 
+inputHeight = prompt("Enter the height");
 let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
 let area = width * height;
+document.write("width:" + width + "<br/>" + "height:" + height + "<br/>" + "the area is: " + area); 
